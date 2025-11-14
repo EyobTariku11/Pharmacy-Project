24c47cf6ab3cfed3fa3bf86f8d94c597e64495b4
Eyob Tariku <eyobtariku111@gmail.com>
2025-11-14 05:51:52 +0000

    Done

--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -64,7 +64,7 @@ export class StockService {
         })
       );
   }
-
+//get all
   getAllStocksPublic(): Observable<Stock[]> {
     return this.http.get<Stock[]>(`${this.apiUrl}/all`)
       .pipe(
